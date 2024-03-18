df57377ecfd3a64b67f07fdd8ef087dfb7c19525
undercover03 <109204278+undercover03@users.noreply.github.com>
2024-03-18 15:35:48 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,9 @@
+function bulbOn()
+{
+    document.getElementById("img").src = "./light-bulb-on-off-png-11553940186lbyqngqg1y.png";
+}
+function bulbOff()
+{
+    document.getElementById("img").src="./light-bulb-on-off-png-115539402943y50vxr5yi.png"
+}
+
